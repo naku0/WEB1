89dcf425dec1c9abbbe148139356f20313cf879d
Alex <409073@niuitmo.ru>
2024-09-23 14:13:04 +0300

    removed regex'es and added new validator w Promises

--- front/scripts.js
+++ front/scripts.js
@@ -6,22 +6,55 @@ function submitForm(event) {
     const y = document.getElementById("y");
     const r = document.getElementById("r");
     if (!x) {
+        createError("x не определен");
     } else {
-        if (((-3)<=y.value && y.value<=5) && (1<=r.value&& r.value<=4)) {
-            sendData(x,y,r);
-        }else{
-            y.classList.add("wrong");
-            r.classList.add("wrong");
-        }
+        y.classList.remove("wrong");
+        r.classList.remove("wrong");
+        Promise.all([
+            checkY(y),
+            checkR(r)
+        ]).then(() =>{
+            sendData(x, y, r);
+        }).catch((error) => {
+            createError(error);
+        });
     }
 }
 
+const checkY = (value) => {
+    return new Promise((resolve, reject) => {
+        if ((-3) > value.value || value.value > 5) {
+            value.classList.add("wrong");
+            reject("y вне допустимого радиуса");
+        } else {
+            resolve();
+        }
+    });
+}
 
+const checkR = (value) => {
+    return new Promise((resolve, reject) => {
+        if (1 > value.value || value.value > 4) {
+            value.classList.add("wrong");
+            reject("r вне допустимого радиуса")
+        } else {
+            resolve();
+        }
+    });
+}
+
+const createError = (message) =>{
+    document.querySelectorAll(".error").forEach(el => el.remove());
+    const error = document.createElement("p");
+    error.className = "error";
+    error.textContent = message;
+    document.getElementById("coordInputs").prepend(error);
+}
 
 function sendData(x, y, r) {
     fetch(url, {
         "method": 'POST',
-        "header":{
+        "header": {
             'Content-Type': 'application/json'
         },
         "body": JSON.stringify(
@@ -34,7 +67,7 @@ function sendData(x, y, r) {
     }).then(response => {
         response.json()
             .then(data => {
-                addToTable(x.value,y.value,r.value,data.status,data.time,new Date().toLocaleTimeString());
+                addToTable(x.value, y.value, r.value, data.status, data.time, new Date().toLocaleTimeString());
                 console.log("row added");
                 drawDot(x.value, y.value, r.value, data.status);
                 y.classList.remove("wrong");
@@ -42,33 +75,34 @@ function sendData(x, y, r) {
             });
     });
 }
-function addToTable(x, y, r, status, time, data){
+
+function addToTable(x, y, r, status, time, data) {
     const tableBody = document.getElementById("table-body");
     const row = document.createElement("tr");
     row.className = "row";
     const xtd = document.createElement("td");
-    xtd.className ="item";
-    xtd.textContent =x;
+    xtd.className = "item";
+    xtd.textContent = x;
     row.appendChild(xtd);
     const ytd = document.createElement("td");
-    ytd.className ="item";
-    ytd.textContent =y;
+    ytd.className = "item";
+    ytd.textContent = y;
     row.appendChild(ytd);
     const rtd = document.createElement("td");
-    rtd.className ="item";
-    rtd.textContent =r;
+    rtd.className = "item";
+    rtd.textContent = r;
     row.appendChild(rtd);
     const stattd = document.createElement("td");
-    stattd.className ="item";
-    stattd.textContent =status;
+    stattd.className = "item";
+    stattd.textContent = status;
     row.appendChild(stattd);
     const timetd = document.createElement("td");
-    timetd.className ="item";
-    timetd.textContent =time;
+    timetd.className = "item";
+    timetd.textContent = time;
     row.appendChild(timetd);
     const datatd = document.createElement("td");
-    datatd.className ="item";
-    datatd.textContent =data;
+    datatd.className = "item";
+    datatd.textContent = data;
     row.appendChild(datatd);
     tableBody.prepend(row);
 }
@@ -76,10 +110,10 @@ function addToTable(x, y, r, status, time, data){
 function drawDot(x, y, r, status) {
     const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
-    const formula = (coord, radius) => (200 + (4*coord * 40)/radius);
+    const formula = (coord, radius) => (200 + (4 * coord * 40) / radius);
     ctx.beginPath();
     ctx.fillStyle = status ? '#A8E4A0' : '#EE204D';
-    ctx.moveTo(200,200);
+    ctx.moveTo(200, 200);
     ctx.arc(formula(x, r), formula(-y, r), 5, 0, 2 * Math.PI);
     ctx.fill();
     ctx.closePath();
